4d43c3b4afaf5811d73f161904f116062abc92ae
Juan Carlos Romero <59720743+juancarlos736@users.noreply.github.com>
2023-06-05 08:43:19 -0500

    commit2

---
+++ Modelo/Usuario.js
@@ -0,0 +1,19 @@
+class Usuario{
+    constructor() {
+        this.nombreDeUsuario;
+        this.contrasenia;
+        this.id;
+        this.correo;
+    }
+    iniciarSesion(correo,contrasenia){
+
+    }
+    enviarMensaje(mensaje,Destinatario){
+
+    }
+    registrarUsuario(correo,nombreUsuario, contrasenia){
+
+    }
+
+
+}
